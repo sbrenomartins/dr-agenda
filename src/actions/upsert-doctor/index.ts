4e5eac7ea0d344034c6ccd2be2294613cae0ac28
Breno M. Silva <sbrenomartins@gmail.com>
2025-06-04 17:51:10 -0300

    feat: ensure doctors are listed on doctors page

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -2,6 +2,7 @@
 
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -60,4 +61,5 @@ export const upsertDoctor = actionClient
           availableToTime: availableToTimeUTC.format("HH:mm:ss"),
         },
       });
+    revalidatePath("/doctors");
   });
